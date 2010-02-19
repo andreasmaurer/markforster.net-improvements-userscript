ae5d68519deb6df93e6ff903393e719e31eceb90
Andreas Hofmann <andreas@autofocus.cc>
2010-02-19 20:53:11 +0100

    Added "jump to last page", "jump to bottom" and "open in new tab/window" functionality

--- mf.net-improvement.user.js
+++ mf.net-improvement.user.js
@@ -5,18 +5,44 @@
 // @include        http://www.markforster.net/*
 // ==/UserScript==
 
-var isEscBlocked;
+var isEscBlocked, isOpenInNewWindow, isJumpToLastPage, isJumpToBottom;
 
 loadSettings();
 createMenu();
 
-if (isEscBlocked == '1') BlockEsc();
+if (isEscBlocked == '1') {
+  blockEsc();
+}
+
+if (document.location == 'http://www.markforster.net/forum' || document.location == 'http://www.markforster.net/forum/') {
+  if (isOpenInNewWindow == '1'
+  || isJumpToLastPage == '1'
+  || isJumpToBottom == '1') {
+    modifyLinks();
+  }
+}
+
 
 function loadSettings() {
   isEscBlocked = loadSetting('isEscBlocked');
   if (isEscBlocked === '') {
     isEscBlocked = 1;
     saveSetting('isEscBlocked', isEscBlocked);
+  }
+  isOpenInNewWindow = loadSetting('isOpenInNewWindow');
+  if (isOpenInNewWindow === '') {
+    isOpenInNewWindow = 0;
+    saveSetting('isOpenInNewWindow', isOpenInNewWindow);
+  }
+  isJumpToLastPage = loadSetting('isJumpToLastPage');
+  if (isJumpToLastPage === '') {
+    isJumpToLastPage = 1;
+    saveSetting('isJumpToLastPage', isJumpToLastPage);
+  }
+  isJumpToBottom = loadSetting('isJumpToBottom');
+  if (isJumpToBottom === '') {
+    isJumpToBottom = 1;
+    saveSetting('isJumpToBottom', isJumpToBottom);
   }
 }
 
@@ -27,17 +53,40 @@ function createMenu() {
   menuBox.innerHTML = '<div class="iw-s1"><div class="iw-s2">'
                     + '<div class="iw-s3"><div class="iw-s4">'
                     + '<div class="section">'
-                    + '<div class="caption">Options</div>'
+                    + '<div class="caption">Userscript Options</div>'
                     + '<div class="content-passthrough">'
                     + '<div id="imp" class="widget-wrapper widget-type-page">'
+                    + '<b>Forum</b><br />'
+                    + '<input type="checkbox"'
+                    + (isJumpToLastPage == '1' ? ' checked="checked"' : '')
+                    + ' id="checkJumpToLastPage" /> '
+                    + '<label for="checkJumpToLastPage">Open Topic: Go to Last Page</label>'
+                    + '<br /><input type="checkbox"'
+                    + (isJumpToBottom == '1' ? ' checked="checked"' : '')
+                    + ' id="checkJumpToBottom" /> '
+                    + '<label for="checkJumpToBottom">Open Topic: Jump to Bottom</label>'
+                    + '<br /><input type="checkbox"'
+                    + (isOpenInNewWindow == '1' ? ' checked="checked"' : '')
+                    + ' id="checkOpenInNewWindow" /> '
+                    + '<label for="checkOpenInNewWindow">Open Topic: New Tab/Window</label>'
+                    + '<br /><b>General</b><br />'
                     + '<input type="checkbox"'
                     + (isEscBlocked == '1' ? ' checked="checked"' : '')
                     + ' id="checkEscBlocked" /> '
                     + '<label for="checkEscBlocked">Block Esc Key Login</label>'
+                    + '<p style="font-size:10px;margin-top:5px;">'
+                    + 'Copyright &copy; 2010 <a href="http://andreashofmann.info/">Andreas Hofmann</a>'
+                    + '</p>'
                     + '</div></div></div></div></div></div></div></div>';
   sidebar1.appendChild(menuBox);
   var checkbox = document.getElementById('checkEscBlocked');
   checkbox.addEventListener('click', toggleEscBlocked, false);
+  var checkbox = document.getElementById('checkJumpToLastPage');
+  checkbox.addEventListener('click', toggleJumpToLastPage, false);
+  var checkbox = document.getElementById('checkJumpToBottom');
+  checkbox.addEventListener('click', toggleJumpToBottom, false);
+  var checkbox = document.getElementById('checkOpenInNewWindow');
+  checkbox.addEventListener('click', toggleOpenInNewWindow, false);
 }
 
 function loadSetting(setting) {
@@ -65,17 +114,17 @@ function toggleEscBlocked() {
   isEscBlocked = checkbox.checked ? '1':'0'
   saveSetting('isEscBlocked', isEscBlocked);
   if (isEscBlocked == '1') {
-    BlockEsc();
+    blockEsc();
   } else {
-    UnblockEsc();
+    unblockEsc();
   }
 }
 
-function BlockEsc() {
+function blockEsc() {
   window.addEventListener('keyup', keyHandler, true);
 }
 
-function UnblockEsc() {
+function unblockEsc() {
   window.removeEventListener('keyup', keyHandler, true);
 }
 
@@ -85,6 +134,53 @@ function keyHandler(event) {
     return true; 
   }
   return false;
+}
+
+function toggleJumpToLastPage() {
+  var checkbox = document.getElementById('checkJumpToLastPage');
+  isJumpToLastPage = checkbox.checked ? '1':'0'
+  saveSetting('isJumpToLastPage', isJumpToLastPage);
+  modifyLinks();
+}
+
+function toggleJumpToBottom() {
+  var checkbox = document.getElementById('checkJumpToBottom');
+  isJumpToBottom = checkbox.checked ? '1':'0'
+  saveSetting('isJumpToBottom', isJumpToBottom);
+  modifyLinks();
+}
+
+function toggleOpenInNewWindow() {
+  var checkbox = document.getElementById('checkOpenInNewWindow');
+  isOpenInNewWindow = checkbox.checked ? '1':'0'
+  saveSetting('isOpenInNewWindow', isOpenInNewWindow);
+  modifyLinks();
+}
+
+function modifyLinks() {
+  var topicLinks = document.evaluate("//a[contains(@href,'/forum/post/')]",
+    document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+  for (var i = 0; i < topicLinks.snapshotLength; i++) {
+    modifyLink(topicLinks.snapshotItem(i));
+  }
+}
+
+function modifyLink(link) {
+  var address = link.getAttribute('href');
+  if (address.indexOf('?') != -1) {
+    address = address.substring(0, address.indexOf('?'));
+  }
+  if (address.indexOf('#') != -1) {
+    address = address.substring(0, address.indexOf('#'));
+  }
+  link.setAttribute('href', address 
+    + (isJumpToLastPage == '1' ? '?lastPage=true' : '')
+    + (isJumpToBottom == '1' ? '#new-post-button' : ''));
+  if (isOpenInNewWindow == '1') {
+    link.setAttribute('target', '_blank');
+  } else {
+    link.removeAttribute('target');  
+  }
 }
 
 
